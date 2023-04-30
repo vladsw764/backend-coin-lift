50bb299cbc7f36869d533f541b2371dcc3cade8f
vladsw764 <vladsw764@icloud.com>
2023-04-30 11:59:51 +0300

    added CryptocurrencyController

---
+++ src/main/java/com/coinlift/backend/controllers/CryptocurrencyController.java
@@ -0,0 +1,30 @@
+package com.coinlift.backend.controllers;
+
+import com.coinlift.backend.pojo.CryptoPercentData;
+import com.coinlift.backend.pojo.CryptocurrencyNews;
+import com.coinlift.backend.services.CryptoPercentDataService;
+import com.coinlift.backend.services.CryptocurrencyNewsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/cryptocurrency")
+public class CryptocurrencyController {
+    private final CryptoPercentDataService dataService;
+    private final CryptocurrencyNewsService newsService;
+
+    @GetMapping("/percent-data")
+    public List<CryptoPercentData> getCryptoPercentData() {
+        return dataService.getCryptoPercentData();
+    }
+
+    @GetMapping("/news")
+    public List<CryptocurrencyNews> getCryptocurrencyNews() {
+        return newsService.getCoinDeskArticles();
+    }
+}
