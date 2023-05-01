2b5ceff73687b72764100c7796837a4cbe51ef20
vladsw764 <vladsw764@icloud.com>
2023-05-01 11:14:22 +0300

    added repositories for Comment and Post entities

---
+++ src/main/java/com/coinlift/backend/repositories/PostRepository.java
@@ -0,0 +1,7 @@
+package com.coinlift.backend.repositories;
+
+import com.coinlift.backend.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
