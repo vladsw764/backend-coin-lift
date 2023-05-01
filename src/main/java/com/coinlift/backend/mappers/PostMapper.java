98ec1e6d037cd2aecf8f130d9cec61c984570876
vladsw764 <vladsw764@icloud.com>
2023-05-01 12:58:58 +0300

    added PostMapper

---
+++ src/main/java/com/coinlift/backend/mappers/PostMapper.java
@@ -0,0 +1,27 @@
+package com.coinlift.backend.mappers;
+
+import com.coinlift.backend.dtos.posts.PostRequestDto;
+import com.coinlift.backend.dtos.posts.PostResponseDto;
+import com.coinlift.backend.entities.Post;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostMapper {
+    public static PostResponseDto toPostResponseDto(Post post) {
+        return PostResponseDto.builder()
+                .title(post.getTitle())
+                .content(post.getContent())
+                .imageLink(post.getImageLink())
+                .createdAt(post.getCreatedAt())
+                .comments(post.getComments().stream().map(CommentMapper::toCommentResponseDto).toList())
+                .build();
+    }
+
+    public static Post toPostEntity(PostRequestDto postRequestDto) {
+        return Post.builder()
+                .title(postRequestDto.title())
+                .content(postRequestDto.content())
+                .imageLink(postRequestDto.imageLink())
+                .build();
+    }
+}
