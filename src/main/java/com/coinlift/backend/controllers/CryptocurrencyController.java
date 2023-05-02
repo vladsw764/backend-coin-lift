5d1de6699dad5efd544d41ca4eb700fd207e9eb2
vladsw764 <vladsw764@icloud.com>
2023-05-02 10:34:42 +0300

    added new Endpoint for events

--- src/main/java/com/coinlift/backend/controllers/CryptocurrencyController.java
+++ src/main/java/com/coinlift/backend/controllers/CryptocurrencyController.java
@@ -1,10 +1,13 @@
 package com.coinlift.backend.controllers;
 
+import com.coinlift.backend.pojo.CryptoEvent;
 import com.coinlift.backend.pojo.CryptoPercentData;
 import com.coinlift.backend.pojo.CryptocurrencyNews;
+import com.coinlift.backend.services.apis.CryptoEventsService;
 import com.coinlift.backend.services.apis.CryptoPercentDataService;
 import com.coinlift.backend.services.apis.CryptocurrencyNewsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,14 +20,20 @@ import java.util.List;
 public class CryptocurrencyController {
     private final CryptoPercentDataService dataService;
     private final CryptocurrencyNewsService newsService;
+    private final CryptoEventsService eventsService;
 
     @GetMapping("/percent-data")
-    public List<CryptoPercentData> getCryptoPercentData() {
-        return dataService.getCryptoPercentData();
+    public ResponseEntity<List<CryptoPercentData>> getCryptoPercentData() {
+        return ResponseEntity.ok(dataService.getCryptoPercentData());
     }
 
     @GetMapping("/news")
-    public List<CryptocurrencyNews> getCryptocurrencyNews() {
-        return newsService.getCoinDeskArticles();
+    public ResponseEntity<List<CryptocurrencyNews>> getCryptocurrencyNews() {
+        return ResponseEntity.ok(newsService.getCoinDeskArticles());
+    }
+
+    @GetMapping("/events")
+    public ResponseEntity<List<CryptoEvent>> getCryptoEvent() {
+        return ResponseEntity.ok(eventsService.getLatestCryptoEvents());
     }
 }
