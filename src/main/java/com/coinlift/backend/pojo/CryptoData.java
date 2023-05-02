091b2bb2d43d74128d92a3a06250d6858e924b70
vladsw764 <vladsw764@icloud.com>
2023-05-02 15:23:53 +0300

    created CryptoData POJO

---
+++ src/main/java/com/coinlift/backend/pojo/CryptoData.java
@@ -0,0 +1,19 @@
+package com.coinlift.backend.pojo;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Builder
+@Setter
+public class CryptoData {
+    private String imageUrl;
+    private String name;
+    private double price;
+    private double percentChange1H;
+    private double percentChange1D;
+    private double percentChange1W;
+    private double marketCap;
+    private long volumeUSD;
+    private long circulatingSupply;
+}
