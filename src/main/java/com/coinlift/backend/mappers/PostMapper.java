45d2bc9c9fb73b49aaec02a025d84f74a78a19c9
vladsw764 <vladsw764@icloud.com>
2023-05-02 11:51:23 +0300

    replace new method 'toPostDetailsResponseDto'

--- src/main/java/com/coinlift/backend/mappers/PostMapper.java
+++ src/main/java/com/coinlift/backend/mappers/PostMapper.java
@@ -1,5 +1,6 @@
 package com.coinlift.backend.mappers;
 
+import com.coinlift.backend.dtos.posts.PostDetailsResponseDto;
 import com.coinlift.backend.dtos.posts.PostRequestDto;
 import com.coinlift.backend.dtos.posts.PostResponseDto;
 import com.coinlift.backend.entities.Post;
@@ -7,13 +8,22 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PostMapper {
+    public static PostDetailsResponseDto toPostDetailsResponseDto(Post post) {
+        return PostDetailsResponseDto.builder()
+                .title(post.getTitle())
+                .content(post.getContent())
+                .imageLink(post.getImageLink())
+                .createdAt(post.getCreatedAt())
+                .comments(post.getComments().stream().map(CommentMapper::toCommentResponseDto).toList())
+                .build();
+    }
+
     public static PostResponseDto toPostResponseDto(Post post) {
         return PostResponseDto.builder()
                 .title(post.getTitle())
                 .content(post.getContent())
                 .imageLink(post.getImageLink())
                 .createdAt(post.getCreatedAt())
-                .comments(post.getComments().stream().map(CommentMapper::toCommentResponseDto).toList())
                 .build();
     }
 
