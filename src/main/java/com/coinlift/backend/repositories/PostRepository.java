963503dce0adb378b8185a683730f16504d7225f
vladsw764 <vladsw764@icloud.com>
2023-05-03 14:05:10 +0300

    deleted 'findById()' method

--- src/main/java/com/coinlift/backend/repositories/PostRepository.java
+++ src/main/java/com/coinlift/backend/repositories/PostRepository.java
@@ -5,12 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
-    Optional<Post> findById(UUID id);
-
+public interface PostRepository extends JpaRepository<Post, UUID> {
     @Query(value = "SELECT * FROM posts LIMIT 6", nativeQuery = true)
     List<Post> findLatestPosts();
 
