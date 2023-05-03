e94b948c50d1495f1de2e50ecfd8c66274252cc5
vladsw764 <vladsw764@icloud.com>
2023-05-03 13:02:47 +0300

    Add pagination support to getPostById method

--- src/main/java/com/coinlift/backend/services/posts/PostService.java
+++ src/main/java/com/coinlift/backend/services/posts/PostService.java
@@ -3,13 +3,14 @@ package com.coinlift.backend.services.posts;
 import com.coinlift.backend.dtos.posts.PostDetailsResponseDto;
 import com.coinlift.backend.dtos.posts.PostRequestDto;
 import com.coinlift.backend.dtos.posts.PostResponseDto;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface PostService {
     List<PostResponseDto> getLatestPosts();
-    PostDetailsResponseDto getPostById(UUID postId);
+    PostDetailsResponseDto getPostById(UUID postId, Pageable pageable);
     PostResponseDto removePost(UUID postId);
     UUID createPost(PostRequestDto postRequestDto);
     PostResponseDto updatePost(UUID postId);
