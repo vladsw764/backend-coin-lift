86bf7c8d1e4655b7c1b9eebfc5ae586a9d84a784
vladsw764 <vladsw764@icloud.com>
2023-05-03 13:07:33 +0300

    add method 'findAllByPostId()' which include 'Pageable'

--- src/main/java/com/coinlift/backend/repositories/CommentRepository.java
+++ src/main/java/com/coinlift/backend/repositories/CommentRepository.java
@@ -1,9 +1,12 @@
 package com.coinlift.backend.repositories;
 
 import com.coinlift.backend.entities.Comment;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
+    List<Comment> findAllByPostId(UUID postId, Pageable pageable);
 }
