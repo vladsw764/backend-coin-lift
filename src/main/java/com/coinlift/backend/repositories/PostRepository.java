85930172a15f25ab8a23800cc0975cbc0ee23bbe
vladsw764 <vladsw764@icloud.com>
2023-05-03 10:22:56 +0300

    rewritten method 'findById()' and added 'findLatestPosts()'

--- src/main/java/com/coinlift/backend/repositories/PostRepository.java
+++ src/main/java/com/coinlift/backend/repositories/PostRepository.java
@@ -2,6 +2,17 @@ package com.coinlift.backend.repositories;
 
 import com.coinlift.backend.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Query("select p from Post p where p.id = ?1")
+    Optional<Post> findById(UUID postId);
+
+    @Query(value = "SELECT * FROM posts LIMIT 6", nativeQuery = true)
+    List<Post> findLatestPosts();
+
 }
