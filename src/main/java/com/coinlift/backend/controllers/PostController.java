e400bf6a0d66caac0aeb882bfae4eb0787c41adc
vladsw764 <vladsw764@icloud.com>
2023-05-03 14:06:20 +0300

    implemented basic endpoint for Post Entity

---
+++ src/main/java/com/coinlift/backend/controllers/PostController.java
@@ -0,0 +1,52 @@
+package com.coinlift.backend.controllers;
+
+import com.coinlift.backend.dtos.posts.PostDetailsResponseDto;
+import com.coinlift.backend.dtos.posts.PostRequestDto;
+import com.coinlift.backend.dtos.posts.PostResponseDto;
+import com.coinlift.backend.services.posts.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
+public class PostController {
+    private final PostService postService;
+
+    @GetMapping("/latest")
+    public ResponseEntity<List<PostResponseDto>> getLatestPosts() {
+        return new ResponseEntity<>(postService.getLatestPosts(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<UUID> createPost(@RequestBody PostRequestDto postRequestDto) {
+        return new ResponseEntity<>(postService.createPost(postRequestDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping({"{uuid}"})
+    public ResponseEntity<PostDetailsResponseDto> getPost(@PathVariable(name = "uuid") UUID uuid,
+                                                          @RequestParam(defaultValue = "0") int page,
+                                                          @RequestParam(defaultValue = "5") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return new ResponseEntity<>(postService.getPostById(uuid, pageable), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{uuid}")
+    public ResponseEntity<String> removePostById(@PathVariable(name = "uuid") UUID uuid) {
+        postService.removePost(uuid);
+        return ResponseEntity.ok("Entity deleted");
+    }
+
+    @PutMapping("/{uuid}")
+    public ResponseEntity<PostResponseDto> updatePostById(@PathVariable(name = "uuid") UUID uuid,
+                                                          @RequestBody PostRequestDto postRequestDto) {
+        return new ResponseEntity<>(postService.updatePost(uuid, postRequestDto), HttpStatus.OK);
+    }
+}
