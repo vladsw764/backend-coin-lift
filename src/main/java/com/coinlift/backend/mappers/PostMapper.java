aad349718176cc192d75eaca450c3241f671de34
vladsw764 <vladsw764@icloud.com>
2023-05-03 10:12:36 +0300

    minor edits in PostMapper

--- src/main/java/com/coinlift/backend/mappers/PostMapper.java
+++ src/main/java/com/coinlift/backend/mappers/PostMapper.java
@@ -4,21 +4,24 @@ import com.coinlift.backend.dtos.posts.PostDetailsResponseDto;
 import com.coinlift.backend.dtos.posts.PostRequestDto;
 import com.coinlift.backend.dtos.posts.PostResponseDto;
 import com.coinlift.backend.entities.Post;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class PostMapper {
-    public static PostDetailsResponseDto toPostDetailsResponseDto(Post post) {
+    private final CommentMapper commentMapper;
+    public PostDetailsResponseDto toPostDetailsResponseDto(Post post) {
         return PostDetailsResponseDto.builder()
                 .title(post.getTitle())
                 .content(post.getContent())
                 .imageLink(post.getImageLink())
                 .createdAt(post.getCreatedAt())
-                .comments(post.getComments().stream().map(CommentMapper::toCommentResponseDto).toList())
+                .comments(post.getComments().stream().map(commentMapper::toCommentResponseDto).toList())
                 .build();
     }
 
-    public static PostResponseDto toPostResponseDto(Post post) {
+    public PostResponseDto toPostResponseDto(Post post) {
         return PostResponseDto.builder()
                 .title(post.getTitle())
                 .content(post.getContent())
@@ -27,7 +30,7 @@ public class PostMapper {
                 .build();
     }
 
-    public static Post toPostEntity(PostRequestDto postRequestDto) {
+    public Post toPostEntity(PostRequestDto postRequestDto) {
         return Post.builder()
                 .title(postRequestDto.title())
                 .content(postRequestDto.content())
