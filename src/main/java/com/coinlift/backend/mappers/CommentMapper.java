f2cd236b86f4279a9d77eef83e47754477922541
vladsw764 <vladsw764@icloud.com>
2023-05-04 14:42:44 +0300

    included post repository and edited 'toCommentEntity' method with using 'postRepository.findById()'

--- src/main/java/com/coinlift/backend/mappers/CommentMapper.java
+++ src/main/java/com/coinlift/backend/mappers/CommentMapper.java
@@ -3,11 +3,18 @@ package com.coinlift.backend.mappers;
 import com.coinlift.backend.dtos.comments.CommentRequestDto;
 import com.coinlift.backend.dtos.comments.CommentResponseDto;
 import com.coinlift.backend.entities.Comment;
-import com.coinlift.backend.entities.Post;
+import com.coinlift.backend.exceptions.ResourceNotFoundException;
+import com.coinlift.backend.repositories.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
+@RequiredArgsConstructor
 public class CommentMapper {
+    private final PostRepository postRepository;
+
     public CommentResponseDto toCommentResponseDto(Comment comment) {
         return CommentResponseDto.builder()
                 .id(comment.getId())
@@ -16,10 +23,12 @@ public class CommentMapper {
                 .build();
     }
 
-    public Comment toCommentEntity(CommentRequestDto commentRequestDto, Post post) {
+    public Comment toCommentEntity(CommentRequestDto commentRequestDto, UUID postId) {
         return Comment.builder()
                 .content(commentRequestDto.content())
-                .post(post)
+                .post(postRepository
+                        .findById(postId)
+                        .orElseThrow(() -> new ResourceNotFoundException("post with id" + postId + "not found")))
                 .build();
     }
 }
