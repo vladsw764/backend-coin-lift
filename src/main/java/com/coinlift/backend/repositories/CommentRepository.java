4dcb3baa3305374279ffc5a5cf478f6803a57e50
vladsw764 <vladsw764@icloud.com>
2023-05-04 14:40:25 +0300

    added method 'findByPostIdAndCommentId'

--- src/main/java/com/coinlift/backend/repositories/CommentRepository.java
+++ src/main/java/com/coinlift/backend/repositories/CommentRepository.java
@@ -3,10 +3,16 @@ package com.coinlift.backend.repositories;
 import com.coinlift.backend.entities.Comment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
+    @Query("SELECT c FROM Comment c WHERE c.post.id = ?1 ORDER BY c.createdAt DESC")
     List<Comment> findAllByPostId(UUID postId, Pageable pageable);
+
+    @Query("SELECT c FROM Comment c WHERE c.post.id = ?1 AND c.id = ?2")
+    Optional<Comment> findByPostIdAndCommentId(UUID postId, UUID commentId);
 }
