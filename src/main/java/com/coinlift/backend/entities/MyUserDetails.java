073c6ba157920edb4932acac3861c866f9dc484f
Vladyslav <vladsw764@icloud.com>
2023-05-21 18:15:08 +0300

    minor edits in MyUserDetails class

--- src/main/java/com/coinlift/backend/entities/MyUserDetails.java
+++ src/main/java/com/coinlift/backend/entities/MyUserDetails.java
@@ -34,7 +34,7 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public boolean isAccountNonLocked() {
-        return user.isNotBlocked();
+        return true;
     }
 
     @Override
