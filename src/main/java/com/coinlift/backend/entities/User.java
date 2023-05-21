7bc0a74fa77c862e630f82f09ccafcb1444c0395
Vladyslav <vladsw764@icloud.com>
2023-05-21 12:01:36 +0300

    added User Entity

---
+++ src/main/java/com/coinlift/backend/entities/User.java
@@ -0,0 +1,29 @@
+package com.coinlift.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+    @Column(nullable = false)
+    private boolean isNotBlocked;
+}
