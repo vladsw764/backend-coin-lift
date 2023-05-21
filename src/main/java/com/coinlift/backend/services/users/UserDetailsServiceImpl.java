cd012ccd2143b7b719e2fa6f677e4a545efad8a0
Vladyslav <vladsw764@icloud.com>
2023-05-21 12:01:53 +0300

    Added UserDetailsService

---
+++ src/main/java/com/coinlift/backend/services/users/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.coinlift.backend.services.users;
+
+import com.coinlift.backend.entities.MyUserDetails;
+import com.coinlift.backend.entities.User;
+import com.coinlift.backend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public MyUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new MyUserDetails(user);
+    }
+}
