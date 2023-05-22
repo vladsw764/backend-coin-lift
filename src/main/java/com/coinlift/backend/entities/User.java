5726150d514af1e415639ebffdd217f9f1d2a2e5
Vladyslav <vladsw764@icloud.com>
2023-05-22 10:31:24 +0300

    Added many-to-one relationship between Comment, Post and User entities

--- src/main/java/com/coinlift/backend/entities/User.java
+++ src/main/java/com/coinlift/backend/entities/User.java
@@ -3,6 +3,8 @@ package com.coinlift.backend.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -24,5 +26,8 @@ public class User {
     private String password;
     @Enumerated(value = EnumType.STRING)
     private Role role;
-
+    @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
+    List<Comment> comments = new ArrayList<>();
+    @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
+    List<Post> posts = new ArrayList<>();
 }
