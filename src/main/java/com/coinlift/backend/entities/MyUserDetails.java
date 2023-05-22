5524131850055b4b7fa2773f03c53aae28fcc286
Vladyslav <vladsw764@icloud.com>
2023-05-22 10:25:58 +0300

    minor edits (class -> record)

--- src/main/java/com/coinlift/backend/entities/MyUserDetails.java
+++ src/main/java/com/coinlift/backend/entities/MyUserDetails.java
@@ -1,6 +1,5 @@
 package com.coinlift.backend.entities;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,9 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-@RequiredArgsConstructor
-public class MyUserDetails implements UserDetails {
-    private final User user;
+public record MyUserDetails(User user) implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
