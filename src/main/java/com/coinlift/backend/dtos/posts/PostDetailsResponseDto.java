b8d930eaf68752ab07dcc7d7de65325e535fcc52
Vladyslav <vladsw764@icloud.com>
2023-05-26 16:50:43 +0300

    added fields: 'username', 'isCreator'

--- src/main/java/com/coinlift/backend/dtos/posts/PostDetailsResponseDto.java
+++ src/main/java/com/coinlift/backend/dtos/posts/PostDetailsResponseDto.java
@@ -14,9 +14,11 @@ import java.util.UUID;
 @AllArgsConstructor
 public class PostDetailsResponseDto {
     private UUID uuid;
+    private String username;
     private String title;
     private String content;
     private byte[] image;
     private LocalDateTime createdAt;
     private List<CommentResponseDto> comments;
+    private boolean isCreator;
 }
