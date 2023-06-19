37b448e816600937dd89154763f3b8249297ec2b
Vladyslav <vladsw764@icloud.com>
2023-06-19 13:07:32 +0300

    added CryptoImage entity

---
+++ src/main/java/com/coinlift/backend/entities/CryptoImage.java
@@ -0,0 +1,22 @@
+package com.coinlift.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "crypto_images")
+public class CryptoImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer cryptoId;
+    private String cryptoImageUrl;
+}
