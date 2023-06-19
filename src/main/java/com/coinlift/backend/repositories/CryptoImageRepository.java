b45abb5a31e9aff4fd1916d085386b3836128dba
Vladyslav <vladsw764@icloud.com>
2023-06-19 13:07:43 +0300

    added CryptoImage repository

---
+++ src/main/java/com/coinlift/backend/repositories/CryptoImageRepository.java
@@ -0,0 +1,8 @@
+package com.coinlift.backend.repositories;
+
+import com.coinlift.backend.entities.CryptoImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CryptoImageRepository extends JpaRepository<CryptoImage, Long> {
+    CryptoImage findByCryptoId(Integer id);
+}
