e68587b43bc7c8eff85fc3f20d62e47c3ba60858
Vladyslav <vladsw764@icloud.com>
2023-07-04 18:30:46 +0300

    excluded business logic for getting authentication info

--- src/main/java/com/coinlift/backend/controllers/PostController.java
+++ src/main/java/com/coinlift/backend/controllers/PostController.java
@@ -4,7 +4,6 @@ import com.coinlift.backend.dtos.posts.PostDetailsResponseDto;
 import com.coinlift.backend.dtos.posts.PostRequestDto;
 import com.coinlift.backend.dtos.posts.PostResponseDto;
 import com.coinlift.backend.services.posts.PostService;
-import com.coinlift.backend.services.users.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -24,7 +23,6 @@ import java.util.UUID;
 @CrossOrigin("*")
 public class PostController {
     private final PostService postService;
-    private final JwtService jwtService;
 
     @GetMapping
     public ResponseEntity<List<PostResponseDto>> getAllPosts(@RequestParam(defaultValue = "0") int page,
@@ -40,43 +38,35 @@ public class PostController {
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> createPost(@RequestParam String title,
                                         @RequestParam String content,
-                                        @RequestParam(value = "file", required = false) MultipartFile postImage,
-                                        @RequestHeader("Authorization") String jwt) {
+                                        @RequestParam(value = "file", required = false) MultipartFile postImage) {
 
-        // Check if the uploaded file is an image
         if (postImage != null && !postImage.isEmpty()) {
             if (!Objects.requireNonNull(postImage.getContentType()).startsWith("image/")) {
                 return new ResponseEntity<>("Only image files are allowed!", HttpStatus.UNSUPPORTED_MEDIA_TYPE);
             }
         }
         PostRequestDto postRequestDto = new PostRequestDto(title, content);
-        UUID userId = jwtService.extractUserIdFromToken(jwt);
-        return new ResponseEntity<>(postService.createPost(postRequestDto, postImage, userId), HttpStatus.CREATED);
+        return new ResponseEntity<>(postService.createPost(postRequestDto, postImage), HttpStatus.CREATED);
     }
 
 
     @GetMapping({"{uuid}"})
     public ResponseEntity<PostDetailsResponseDto> getPost(@PathVariable(name = "uuid") UUID uuid,
                                                           @RequestParam(defaultValue = "0") int page,
-                                                          @RequestParam(defaultValue = "5") int size,
-                                                          @RequestHeader(value = "Authorization", required = false) String jwt) {
+                                                          @RequestParam(defaultValue = "5") int size) {
         Pageable pageable = PageRequest.of(page, size);
-        return new ResponseEntity<>(postService.getPostById(uuid, jwt, pageable), HttpStatus.OK);
+        return new ResponseEntity<>(postService.getPostById(uuid, pageable), HttpStatus.OK);
     }
 
     @DeleteMapping("/{uuid}")
-    public ResponseEntity<String> removePostById(@PathVariable(name = "uuid") UUID uuid,
-                                                 @RequestHeader("Authorization") String jwt) {
-        UUID userId = jwtService.extractUserIdFromToken(jwt);
-        postService.removePost(uuid, userId);
+    public ResponseEntity<String> removePostById(@PathVariable(name = "uuid") UUID uuid) {
+        postService.removePost(uuid);
         return ResponseEntity.ok("Post successfully removed!");
     }
 
     @PatchMapping("/{uuid}")
     public ResponseEntity<PostResponseDto> updatePostById(@PathVariable(name = "uuid") UUID uuid,
-                                                          @RequestBody PostRequestDto postRequestDto,
-                                                          @RequestHeader("Authorization") String jwt) {
-        UUID userId = jwtService.extractUserIdFromToken(jwt);
-        return new ResponseEntity<>(postService.updatePost(uuid, postRequestDto, userId), HttpStatus.OK);
+                                                          @RequestBody PostRequestDto postRequestDto) {
+        return new ResponseEntity<>(postService.updatePost(uuid, postRequestDto), HttpStatus.OK);
     }
 }
