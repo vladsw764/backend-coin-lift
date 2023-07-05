6ba5dc45be8972f1d43fc35536077da50d0db879
Vladyslav <vladsw764@icloud.com>
2023-07-05 12:52:44 +0300

    optimize code by extracting business logic to CommentService

--- src/main/java/com/coinlift/backend/controllers/CommentController.java
+++ src/main/java/com/coinlift/backend/controllers/CommentController.java
@@ -3,7 +3,6 @@ package com.coinlift.backend.controllers;
 import com.coinlift.backend.dtos.comments.CommentRequestDto;
 import com.coinlift.backend.dtos.comments.CommentResponseDto;
 import com.coinlift.backend.services.comments.CommentService;
-import com.coinlift.backend.services.users.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,31 +17,24 @@ import java.util.UUID;
 public class CommentController {
 
     private final CommentService commentService;
-    private final JwtService jwtService;
 
     @PostMapping
     public ResponseEntity<UUID> createComment(@RequestBody CommentRequestDto commentRequestDto,
-                                              @PathVariable("postId") UUID uuid,
-                                              @RequestHeader("Authorization") String jwt) {
-        UUID userId = jwtService.extractUserIdFromToken(jwt);
-        return new ResponseEntity<>(commentService.createComment(commentRequestDto, uuid, userId), HttpStatus.CREATED);
+                                              @PathVariable("postId") UUID uuid) {
+        return new ResponseEntity<>(commentService.createComment(commentRequestDto, uuid), HttpStatus.CREATED);
     }
 
     @PatchMapping("{commentId}")
     public ResponseEntity<CommentResponseDto> updateComment(@RequestBody CommentRequestDto commentRequestDto,
                                                             @PathVariable("postId") UUID postId,
-                                                            @PathVariable("commentId") UUID commentId,
-                                                            @RequestHeader("Authorization") String jwt) {
-        UUID userId = jwtService.extractUserIdFromToken(jwt);
-        return new ResponseEntity<>(commentService.updateComment(commentRequestDto, postId, commentId, userId), HttpStatus.OK);
+                                                            @PathVariable("commentId") UUID commentId) {
+        return new ResponseEntity<>(commentService.updateComment(commentRequestDto, postId, commentId), HttpStatus.OK);
     }
 
     @DeleteMapping("{commentId}")
     public ResponseEntity<String> removeComment(@PathVariable("postId") UUID postId,
-                                                @PathVariable("commentId") UUID commentId,
-                                                @RequestHeader("Authorization") String jwt) {
-        UUID userId = jwtService.extractUserIdFromToken(jwt);
-        commentService.deleteComment(postId, commentId, userId);
+                                                @PathVariable("commentId") UUID commentId) {
+        commentService.deleteComment(postId, commentId);
         return ResponseEntity.ok("Comment successfully deleted!");
     }
 }
