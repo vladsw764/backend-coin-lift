f7b406083ad4706c1fa95d9d3d6e7e68d59a0b9e
Vladyslav <vladsw764@icloud.com>
2023-07-05 12:53:28 +0300

    remove userId as a parameter from all methods

--- src/main/java/com/coinlift/backend/services/comments/CommentService.java
+++ src/main/java/com/coinlift/backend/services/comments/CommentService.java
@@ -6,9 +6,9 @@ import com.coinlift.backend.dtos.comments.CommentResponseDto;
 import java.util.UUID;
 
 public interface CommentService {
-    UUID createComment(CommentRequestDto commentRequestDto, UUID postId, UUID userId);
+    UUID createComment(CommentRequestDto commentRequestDto, UUID postId);
 
-    CommentResponseDto updateComment(CommentRequestDto commentRequestDto, UUID postId, UUID commentId, UUID userId);
+    CommentResponseDto updateComment(CommentRequestDto commentRequestDto, UUID postId, UUID commentId);
 
-    void deleteComment(UUID postId, UUID commentId, UUID userId);
+    void deleteComment(UUID postId, UUID commentId);
 }
