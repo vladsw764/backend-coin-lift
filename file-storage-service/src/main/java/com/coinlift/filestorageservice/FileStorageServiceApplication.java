c85a9bf07ce59999e824a58380a404a443b01ffc
Vladyslav <vladsw764@icloud.com>
2023-07-09 15:17:14 +0300

    added main file for FileStorageService

---
+++ file-storage-service/src/main/java/com/coinlift/filestorageservice/FileStorageServiceApplication.java
@@ -0,0 +1,11 @@
+package com.coinlift.filestorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileStorageServiceApplication.class, args);
+    }
+}
