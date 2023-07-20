6c6121655c4f21dca81a4964d9cb0dcd628f06a4
Vladyslav <vladsw764@icloud.com>
2023-07-20 15:56:32 +0300

    remove column 'title'

--- src/main/java/com/coinlift/backend/entities/Post.java
+++ src/main/java/com/coinlift/backend/entities/Post.java
@@ -18,21 +18,26 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Post {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(columnDefinition = "UUID")
     private UUID id;
-    @Column(nullable = false)
-    private String title;
+
     @Column(nullable = false)
     private String content;
+
     private String imageLink;
+
     @CreationTimestamp
     private LocalDateTime createdAt;
+
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+
     @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY, mappedBy = "post", orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
+
     @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "FK_USERS_POSTS"))
     private User user;
