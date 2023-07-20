be5583a764dac7c2946998744c1f685ff9eda4a1
Vladyslav <vladsw764@icloud.com>
2023-07-20 14:55:40 +0300

    add ability to save and revoke JWT token

--- src/main/java/com/coinlift/backend/services/users/AuthenticationServiceImpl.java
+++ src/main/java/com/coinlift/backend/services/users/AuthenticationServiceImpl.java
@@ -2,10 +2,9 @@ package com.coinlift.backend.services.users;
 
 import com.coinlift.backend.dtos.users.UserAuthenticationRequest;
 import com.coinlift.backend.dtos.users.UserRegistrationRequest;
-import com.coinlift.backend.entities.MyUserDetails;
-import com.coinlift.backend.entities.Role;
-import com.coinlift.backend.entities.User;
-import com.coinlift.backend.pojo.AuthenticationResponse;
+import com.coinlift.backend.entities.*;
+import com.coinlift.backend.dtos.users.AuthenticationResponse;
+import com.coinlift.backend.repositories.TokenRepository;
 import com.coinlift.backend.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,11 +12,14 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TokenRepository tokenRepository;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
     private final UserDetailsServiceImpl userDetailsService;
@@ -37,9 +39,11 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 .password(passwordEncoder.encode(userRegistrationRequest.password()))
                 .role(Role.USER)
                 .build();
-        userRepository.save(user);
+        User savedUser = userRepository.save(user);
         MyUserDetails userDetails = new MyUserDetails(user);
         String jwtToken = jwtService.generateToken(userDetails);
+        revokeUserTokens(savedUser);
+        saveUserToken(savedUser, jwtToken);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
@@ -54,9 +58,34 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
         MyUserDetails userDetails = userDetailsService.loadUserByUsername(userRegistrationRequest.getEmail());
         String jwtToken = jwtService.generateToken(userDetails);
-
+        revokeUserTokens(userDetails.user());
+        saveUserToken(userDetails.user(), jwtToken);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
+    }
+
+    private void revokeUserTokens(User user) {
+        List<AuthenticationToken> validUserTokens = tokenRepository.findAllValidTokenByUserId(user.getId());
+        if (validUserTokens.isEmpty())
+            return;
+
+        validUserTokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+
+        tokenRepository.saveAll(validUserTokens);
+    }
+
+    private void saveUserToken(User user, String jwtToken) {
+        var token = AuthenticationToken.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .revoked(false)
+                .expired(false)
+                .build();
+        tokenRepository.save(token);
     }
 }
