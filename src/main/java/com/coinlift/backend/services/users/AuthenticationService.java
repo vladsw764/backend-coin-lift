4610928bdbe6928e7b0e1ac16912440e51cdf706
Vladyslav <vladsw764@icloud.com>
2023-07-20 14:55:04 +0300

    minor edits

--- src/main/java/com/coinlift/backend/services/users/AuthenticationService.java
+++ src/main/java/com/coinlift/backend/services/users/AuthenticationService.java
@@ -2,7 +2,7 @@ package com.coinlift.backend.services.users;
 
 import com.coinlift.backend.dtos.users.UserAuthenticationRequest;
 import com.coinlift.backend.dtos.users.UserRegistrationRequest;
-import com.coinlift.backend.pojo.AuthenticationResponse;
+import com.coinlift.backend.dtos.users.AuthenticationResponse;
 
 public interface AuthenticationService {
     AuthenticationResponse register(UserRegistrationRequest userRegistrationRequest);
