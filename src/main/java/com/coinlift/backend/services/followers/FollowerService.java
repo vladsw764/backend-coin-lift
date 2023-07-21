81659308413b3d2cd46711dad92777dfec11cdb2
Vladyslav <vladsw764@icloud.com>
2023-07-21 13:15:37 +0300

    add follow service

---
+++ src/main/java/com/coinlift/backend/services/followers/FollowerService.java
@@ -0,0 +1,24 @@
+package com.coinlift.backend.services.followers;
+
+import com.coinlift.backend.dtos.users.FollowerResponseDto;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface FollowerService {
+
+    void followUser(UUID followingId);
+
+    void unfollowUser(UUID followingId);
+
+    List<FollowerResponseDto> getFollowers(UUID userId);
+
+    List<FollowerResponseDto> getFollowing(UUID userId);
+
+    int getFollowerCount(UUID userId);
+
+    int getFollowingCount(UUID userId);
+
+    boolean isFollowing(UUID followingId);
+
+}
