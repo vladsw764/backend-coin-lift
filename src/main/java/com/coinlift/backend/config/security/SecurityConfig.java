c0f4bd57bb80124c5aa200bbbb0efd917fedea45
Vladyslav <vladsw764@icloud.com>
2023-07-21 13:15:18 +0300

    permit request

--- src/main/java/com/coinlift/backend/config/security/SecurityConfig.java
+++ src/main/java/com/coinlift/backend/config/security/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                     .requestMatchers("/api/v1/auth/**").permitAll()
                     .requestMatchers(HttpMethod.GET, "/api/v1/posts/**").permitAll()
                     .requestMatchers(HttpMethod.GET, "/api/v1/cryptocurrency/**").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/v1/users/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .sessionManagement()
