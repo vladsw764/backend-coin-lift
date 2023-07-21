06c97efb44945e5e5be67dd5c197fe401647daf4
Vladyslav <vladsw764@icloud.com>
2023-07-21 13:18:13 +0300

    delete unused import

--- src/main/java/com/coinlift/backend/entities/Follower.java
+++ src/main/java/com/coinlift/backend/entities/Follower.java
@@ -1,6 +1,5 @@
 package com.coinlift.backend.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
